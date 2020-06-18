c0c591ed5d88b352b4986016ab2a5db6c8f0c586
Courtney Jones <courtneyrjones95@gmail.com>
2020-06-18 00:48:08 -0500

    added prompts convert strings to numbers and log errors

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
 //My variables
-var includes = [];
-var pLength;
-var uppercase;
-var lowercase;
-var specialCharacters;
+var passLengthString = prompt("How many characters will your password be? Enter a number between 8 and 128");
+var upper = prompt("Would you like your password to contain uppercase values?");
+var lower = prompt("Would you like your password to contain lowercase values?");
+var special = prompt("Would you like your password to contain special characters?");
 
+console.log(typeof passLengthString)
+console.log(upper)
+console.log(lower)
+console.log(special)
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//Turn password length into number
+var passLength = parseInt(passLengthString);
+console.log(passLength)
+
+ //validate password length
+ //broken when inside my function
+ if (passLength < 8 || passLength > 128){
+  console.log("error");
+} else if (passLength != Number){
+  console.log("Not a number");
+}
+
+
 //Collecting password Criteria
-for (var
+function generatePassword (){
+ 
+  
+   
+
+}
   
   
   
@@ -27,3 +48,6 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+
+//get password length
+//validate password length
